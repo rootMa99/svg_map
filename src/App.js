e0730fc5e0d701d1d4ef9aba6148da9827c1fec5
anass zeroual <anasszeroual09@gmail.com>
2024-07-04 09:15:23 +0100

    qsd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Suspense } from 'react';
 import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <Suspense>
+        <Routes>
+          <Route index path="/" element={<Navigate replace to="/home" />} />
+          <Route exact path="/home" element={<h1>homes</h1>} />
+          <Route path="*" element={<Navigate replace to="/home" />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
