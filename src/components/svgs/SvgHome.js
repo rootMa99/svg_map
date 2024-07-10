0f71bc309cc30502675f4f9ab493c6a6fbc2a4d4
anass zeroual <anasszeroual09@gmail.com>
2024-07-10 16:42:06 +0100

    sf

--- src/components/svgs/SvgHome.js
+++ src/components/svgs/SvgHome.js
@@ -1,6 +1,8 @@
 import c from "./SvgHome.module.css";
 
 const SvgHome = (p) => {
-  return <div className={c.container}></div>;
+  return <div className={c.container}>
+  
+  </div>;
 };
 export default SvgHome;
