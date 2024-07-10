b76c16a3e3e2c62a2f88dab78629f99706b1a1df
anass zeroual <anasszeroual09@gmail.com>
2024-07-10 12:22:46 +0100

    dfg

--- src/components/svgs/SvgHome.js
+++ src/components/svgs/SvgHome.js
@@ -1,15 +1,12 @@
 import c from "./SvgHome.module.css";
-import { SvgLoader, SvgProxy } from "react-svgmt";
+
 
 
 
 const SvgHome = (p) => {
   return (
     <div className={c.container}>
-      <SvgLoader path="path-to-your-svg-file.svg">
-        <SvgProxy selector="#some-element" fill="red" />
-        <SvgProxy selector=".some-class" stroke="blue" />
-      </SvgLoader>
+    
     </div>
   );
 };
