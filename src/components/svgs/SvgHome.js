76c7e0c6b5ab0d2bc7e278bb5547144c78859827
anass zeroual <anasszeroual09@gmail.com>
2024-07-11 12:00:19 +0100

    sdf

--- src/components/svgs/SvgHome.js
+++ src/components/svgs/SvgHome.js
@@ -1,35 +1,25 @@
 import c from "./SvgHome.module.css";
 import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
-
+import sv from "../../assets/M4-LAYOUT-EVOLUTION-JULY-2024-Model.svg"
 const SvgHome = (p) => {
     const svgRef = useRef(null);
 
-    useEffect(() => {
-      const svg = d3.select(svgRef.current)
-        .attr('width', 600)
-        .attr('height', 400)
-        .style('border', '1px solid black');
-  
+  useEffect(() => {
+    d3.xml({sv}).then((data) => {
+      const svgNode = data.documentElement;
+      const svg = d3.select(svgRef.current).node().appendChild(svgNode);
+
       const zoom = d3.zoom()
         .scaleExtent([0.5, 5])
         .on('zoom', (event) => {
-          svg.selectAll('g').attr('transform', event.transform);
+          d3.select(svg).selectAll('g').attr('transform', event.transform);
         });
-  
-      svg.call(zoom);
-  
-      const g = svg.append('g');
-      
-      g.append('rect')
-        .attr('width', 100)
-        .attr('height', 100)
-        .attr('x', 50)
-        .attr('y', 50)
-        .attr('class', 'zoom-rect');
-  
-    }, []);
-  
-    return <svg ref={svgRef}></svg>;
+
+      d3.select(svg).call(zoom);
+    });
+  }, []);
+
+  return <div ref={svgRef} className={c.container}></div>;
 };
 export default SvgHome;
