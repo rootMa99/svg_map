7e3f4c43f0042ba27969f119f8f44c65daaf08d0
anass zeroual <anasszeroual09@gmail.com>
2024-07-11 12:23:24 +0100

    ert

--- src/components/svgs/SvgHome.js
+++ src/components/svgs/SvgHome.js
@@ -1,39 +1,46 @@
-import c from "./SvgHome.module.css";
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Snap from 'snapsvg-cjs';
-// import sv from "../../assets/world.svg"
-const SvgHome = (p) => {
-    const svgRef = useRef(null);
+import c from "./SvgHome.module.css";
 
-    useEffect(() => {
-      Snap.load("/assets/M4-LAYOUT-EVOLUTION-JULY-2024-Model.svg", (loadedFragment) => {
-        const s = Snap(svgRef.current);
-        s.append(loadedFragment);
-  
-        let zoomLevel = 1;
-  
-        const zoomIn = () => {
-            console.log("zoom")
-          zoomLevel *= 1.2;
-          s.attr({ viewBox: `0 0 ${600 / zoomLevel} ${400 / zoomLevel}` });
-        };
-  
-        const zoomOut = () => {
-          zoomLevel /= 1.2;
-          s.attr({ viewBox: `0 0 ${600 / zoomLevel} ${400 / zoomLevel}` });
-        };
-  
-        document.getElementById('zoomIn').addEventListener('click', zoomIn);
-        document.getElementById('zoomOut').addEventListener('click', zoomOut);
-      });
-    }, []);
-  
-    return (
-      <div className={c.container}>
-        <div ref={svgRef} className={c["svg-container"]}></div>
-        <button id="zoomIn">Zoom In</button>
-        <button id="zoomOut">Zoom Out</button>
-      </div>
-    );
+const SvgHome = (p) => {
+  const svgRef = useRef(null);
+  const [viewBox, setViewBox] = useState("0 0 600 400");
+
+  useEffect(() => {
+    Snap.load("/assets/nocards.svg", (loadedFragment) => {
+      const s = Snap(svgRef.current);
+      s.append(loadedFragment);
+
+      let zoomLevel = 1;
+
+      const zoomIn = () => {
+        zoomLevel *= 1.2;
+        updateViewBox(zoomLevel);
+      };
+
+      const zoomOut = () => {
+        zoomLevel /= 1.2;
+        updateViewBox(zoomLevel);
+      };
+
+      const updateViewBox = (zoom) => {
+        const width = 600 / zoom;
+        const height = 400 / zoom;
+        setViewBox(`0 0 ${width} ${height}`);
+      };
+
+      document.getElementById('zoomIn').addEventListener('click', zoomIn);
+      document.getElementById('zoomOut').addEventListener('click', zoomOut);
+    });
+  }, []);
+
+  return (
+    <div className={c.container}>
+      <svg ref={svgRef} className={c.svgContainer} viewBox={viewBox}></svg>
+      <button id="zoomIn">Zoom In</button>
+      <button id="zoomOut">Zoom Out</button>
+    </div>
+  );
 };
+
 export default SvgHome;
