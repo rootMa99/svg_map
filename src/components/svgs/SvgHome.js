4e019739ca300c62b1cb84c253041f0c49911e6a
anass zeroual <anasszeroual09@gmail.com>
2024-07-11 12:06:28 +0100

    sdq

--- src/components/svgs/SvgHome.js
+++ src/components/svgs/SvgHome.js
@@ -1,25 +1,31 @@
 import c from "./SvgHome.module.css";
 import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
-import sv from "../../assets/M4-LAYOUT-EVOLUTION-JULY-2024-Model.svg"
+// import sv from "../../assets/world.svg"
 const SvgHome = (p) => {
-    const svgRef = useRef(null);
+    const svgContainerRef = useRef(null);
 
-  useEffect(() => {
-    d3.xml({sv}).then((data) => {
-      const svgNode = data.documentElement;
-      const svg = d3.select(svgRef.current).node().appendChild(svgNode);
-
-      const zoom = d3.zoom()
-        .scaleExtent([0.5, 5])
-        .on('zoom', (event) => {
-          d3.select(svg).selectAll('g').attr('transform', event.transform);
-        });
-
-      d3.select(svg).call(zoom);
-    });
-  }, []);
-
-  return <div ref={svgRef} className={c.container}></div>;
+    useEffect(() => {
+      d3.xml("../../assets/aptiv-logo.svg").then(data => {
+        const importedNode = document.importNode(data.documentElement, true);
+        const svg = d3.select(svgContainerRef.current).node().appendChild(importedNode);
+  
+        const zoom = d3.zoom()
+          .scaleExtent([0.5, 5])
+          .on('zoom', (event) => {
+            d3.select(svg).select('g').attr('transform', event.transform);
+          });
+  
+        d3.select(svg).call(zoom);
+  
+        // Ensure the SVG file has a <g> element to apply the zoom transform
+        if (!d3.select(svg).select('g').node()) {
+          const g = d3.select(svg).append('g');
+          g.selectAll('*').nodes().forEach(node => g.node().appendChild(node));
+        }
+      });
+    }, []);
+  
+    return <div ref={svgContainerRef} className="svg-container"></div>;
 };
 export default SvgHome;
